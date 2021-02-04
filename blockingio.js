c5cca896e88a295c075df5a1593a0d330d680514
AG1L1TYx7 <71088402+AG1L1TYx7@users.noreply.github.com>
2021-02-04 02:30:32 -0800

    Add files via upload

---
+++ blockingio.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+//Synchronous read
+
+var data = fs.readFileSync('input.txt');
+
+// more methods will be bloacked till the read method is not executed
---
+++ nonblocking.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+// Asuyncronous read
+
+fs.readFile('test.txt', function (err, data){});
+
+// more method will execute asycronously
